7ea592dcdbe346d4e2975877fa375e48e061f658
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-06-10 15:19:08 +0800

    add formuser component

--- src/Components/FormUser.tsx
+++ src/Components/FormUser.tsx
@@ -1,3 +1,51 @@
-export const FormUser = () => {
-  return <div>FormUser</div>;
+import React, { useState } from "react";
+import { FormUserDetail } from "./FormUserDetail";
+import { FormPersonalDetail } from "./FormPersonalDetail";
+import { Confirm } from "./Confirm";
+import { Success } from "./Success";
+
+export type User = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  country: string;
+  phone: string;
 };
+
+function FormUser() {
+  // states
+  const [step, setStep] = useState(1);
+  const [user, setUser] = useState<User>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    country: "",
+    phone: "",
+  });
+  // functions
+  function nextStep(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setStep((step) => step + 1);
+  }
+  function prevStep(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setStep((step) => step - 1);
+  }
+  function handelInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser((values) => ({ ...values, [name]: value }));
+  }
+  switch (step) {
+    case 1:
+      return <FormUserDetail />;
+    case 2:
+      return <FormPersonalDetail />;
+    case 3:
+      return <Confirm />;
+    case 4:
+      return <Success />;
+    default:
+      return <div>Error: Invalid step</div>;
+  }
+}
+export default FormUser;
